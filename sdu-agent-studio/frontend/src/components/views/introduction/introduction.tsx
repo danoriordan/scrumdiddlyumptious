9bcaef5cfbb08d7e00bf6046f5472d606ef1bc34
Dan O'Riordan <dan.oriordan@gmail.com>
2024-04-26 15:54:41 +0200

    Adding the Introduction page

--- sdu-agent-studio/frontend/src/components/views/introduction/introduction.tsx
+++ sdu-agent-studio/frontend/src/components/views/introduction/introduction.tsx
@@ -5,20 +5,34 @@ import { navigate } from "gatsby";
 
 // Optional: Import a local image
 import myImage from "../../../images/agents-cpd.webp";  // Adjust the path accordingly
+import myUsecase from "../../../images/usecase.png";  // Adjust the path accordingly
 
 const IntroductionView: React.FC = () => {
 
   return (
-    <div>
-      {/* Image display */}
-      <div style={{ width: '100%', textAlign: 'center', marginBottom: '20px' }}>
-        <img
-          src={myImage} // Change to URL if using an online image
-          alt="Descriptive Alt Text"
-          style={{ width: '50%', height: 'auto' }} // Adjust sizing as needed
-        />
-      </div>
-    </div> // Add a closing curly brace here
+    <React.Fragment>
+      <React.Fragment>
+        <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+          {/* Image display */}
+          <div style={{ width: '50%', textAlign: 'left' }}>
+            <img
+              src={myImage} // Change to URL if using an online image
+              alt="Descriptive Alt Text"
+              style={{ width: '100%', height: 'auto' }} // Adjust sizing as needed
+            />
+          </div>
+          {/* Image display */}
+          <div style={{ width: '50%', textAlign: 'right' }}>
+            <img
+              src={myUsecase} // Change to URL if using an online image
+              alt="Descriptive Alt Text"
+              style={{ width: '100%', height: 'auto' }} // Adjust sizing as needed
+            />
+          </div>
+        </div>
+      </React.Fragment>
+    </React.Fragment>
+
   );
 };
 
